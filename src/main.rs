405ccf2232c6f9b541f45d1f39038bfca7032919
Camilo Schaser-Hughes <camilo.schaserhughes@gmail.com>
2022-06-08 14:03:43 -0700

    add projectile, fmt

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,35 @@
-use monadic::{stdo, state::{State, get, put}};
-use std::time::{Instant, Duration};
+use keyboard_query::{DeviceQuery, DeviceState};
+use monadic::{
+    state::{get, put, State},
+    stdo,
+};
+use std::time::{Duration, Instant};
 
-#[derive(Clone, Debug)]
-struct AppState {
-    solar: Sun,
-}
-#[derive(Clone, Debug)]
+#[derive(Clone, Copy, Debug)]
 struct Sun {
-    time_ch: Instant,
     status: SunShine,
 }
 
 #[derive(Clone, Debug)]
+struct Projectile {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Clone, Debug)]
+struct AppState {
+    time: Instant,
+    sun: Sun,
+    proj: Projectile,
+}
+
+#[derive(Clone, Copy, Debug)]
 enum SunShine {
     Shining,
     Cool,
 }
 
-type St = Sun;
+type St = AppState;
 
 fn initial_state() -> St {
     let a_state: State<'_, St, _> = stdo! {
@@ -25,52 +37,60 @@ fn initial_state() -> St {
         _ <- put(shine);
         pure ("hello")
     };
-    let result = a_state.initial_state(Sun{ time_ch: Instant::now(), 
-        status: SunShine::Cool });
+
+    let result = a_state.initial_state(AppState {
+        time: Instant::now(),
+        sun: Sun {
+            status: SunShine::Cool,
+        },
+        proj: Projectile { x: 9, y: 0 },
+    });
     println!("our initial state of our sun is: {:?}", result);
 
     result.1
 }
 
-fn update_sun(init_st: St) -> St {
-    if init_st.time_ch.elapsed() > Duration::from_secs(1) {
+fn update_state(init_st: St) -> St {
+    if init_st.time.elapsed() > Duration::from_secs(1) {
         let new_time = Instant::now();
         let new_state: State<'_, St, _> = stdo! {
-            shine <- get();
-            let new_shine = match shine.status {
-                SunShine::Cool => (Sun{ time_ch: new_time, status: SunShine::Shining }),
-                SunShine::Shining => (Sun{ time_ch: new_time, status: SunShine::Cool }),
+            state <- get();
+            let new_shine = match state.sun.status {
+                SunShine::Cool => (Sun{ status: SunShine::Shining }),
+                SunShine::Shining => (Sun{ status: SunShine::Cool }),
             };
-            _ <- put(new_shine);
-            // z <- get();
-            pure ("Changing Sun State")
+            let new_proj = Projectile{ x: state.proj.x - 1, y: 0 };
+
+            _ <- put(AppState { time: new_time, sun: new_shine, proj: new_proj });
+            pure ("Changing Game State")
         };
         let newer = new_state.initial_state(init_st);
-        
+
         println!("our new state of our sun is: {:?}", newer);
 
         newer.1
-    } else { init_st }
+    } else {
+        init_st
+    }
 }
+
 fn main() {
     let start = Instant::now();
     println!("Hello World!, this is the MAIN.RS!!!");
     let duration = start.elapsed();
     println!("We've been running this for {:?} amount of time", duration);
 
-
-
-
     let mut result = initial_state();
     println!("our initial state of our sun is: {:?}", result);
-
-    // let newer = update_sun(result);
-    // println!("our new state of our sun is: {:?}", newer);
-
-    // let newest = update_sun(newer);
-    // println!("our new state of our sun is: {:?}", newest);
+    let device_state = DeviceState::new();
+    let mut prev_keys: Vec<u16> = Vec::new();
 
     loop {
-        result = update_sun(result);
+        let keys = device_state.get_keys();
+        if prev_keys != keys && !keys.is_empty() && keys[0] == 32 {
+            println!("space bar pressed at time {:?}", Instant::now());
+        }
+        result = update_state(result);
+        prev_keys = keys;
     }
 }
